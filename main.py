42f7631170e77309c5fd5a6444ed0d37d6f2aff6
WandrV1 <87921354+WandrV1@users.noreply.github.com>
2024-10-30 12:30:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ available_params = [
 ]
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([], md=3),
